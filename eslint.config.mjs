62dc2120e20a06f61cb1657601fdbcddb496a24a
wangxingkang <wang_xingkang@qq.com>
2026-08-19 01:35:59 +0800

    chore: initialize desktop app

---
+++ eslint.config.mjs
@@ -0,0 +1,18 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  stylistic: {
+    semi: true,
+    indent: 2,
+    quotes: 'single',
+  },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  react: true,
+  typescript: true,
+  ignores: [],
+});
