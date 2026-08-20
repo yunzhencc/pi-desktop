774780bb23b9a7ecf878327dea57e34c0eee61ea
wangxingkang <wang_xingkang@qq.com>
2026-08-20 23:12:02 +0800

    refactor: remove legacy theme provider

--- src/renderer/src/components/theme-provider.tsx
+++
@@ -1,6 +0,0 @@
-import type { ThemeProviderProps } from 'next-themes';
-import { ThemeProvider as NextThemesProvider } from 'next-themes';
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
-}
